528f4a0c16f0b0494efa7c4561d8463981e926d8
Saket Nerurkar <saketanerurkar@gmail.com>
2024-12-24 11:55:07 +0530

    Able to create the yml for students and store in scripts folder

--- scripts/populateStudentYMLFile.py
+++ scripts/populateStudentYMLFile.py
@@ -1,1 +1,21 @@
-import numpy as np
+import csv
+import yaml
+import pandas as pd
+
+# Read CSV file
+csv_file = '../_data/students.csv'
+yaml_file = './students.yml'
+
+# Read CSV file into DataFrame
+df = pd.read_csv(csv_file)
+
+# Rename columns
+df.rename(columns={'Name':'name','Projects involved in (separated by ;)': 'info','Years in SenseLab(from-to)': 'yr','link to your url':'link'}, inplace=True)
+
+# Convert DataFrame to list of dictionaries
+data = df.to_dict(orient='records')
+print(data)
+
+# Write to YAML file with ordered columns
+with open(yaml_file, mode='w') as outfile:
+    yaml.dump(data, outfile, default_flow_style=False, sort_keys=False)
