3d5daa0a122d957975ef734fd7e3799a5269c115
Saket Nerurkar <saketanerurkar@gmail.com>
2024-12-24 12:02:15 +0530

    sorted student names alphabetically

--- scripts/populateStudentYMLFile.py
+++ scripts/populateStudentYMLFile.py
@@ -1,16 +1,22 @@
 import csv
 import yaml
 import pandas as pd
+import os
 
-# Read CSV file
-csv_file = '../_data/students.csv'
-yaml_file = '../_data/students.yml'
+# Determine the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Read CSV file into DataFrame
+# Define the paths to the CSV and YAML files relative to the script directory
+csv_file = os.path.join(script_dir, '../_data/students.csv')
+yaml_file = os.path.join(script_dir, '../_data/students.yml')
+
 df = pd.read_csv(csv_file)
 
-# Rename columns
+# Rename columns to match the YAML specifications
 df.rename(columns={'Name':'name','Projects involved in (separated by ;)': 'info','Years in SenseLab(from-to)': 'yr','link to your url':'link'}, inplace=True)
+
+# Sort the DataFrame by the 'name' column
+df.sort_values(by='name', inplace=True)
 
 # Convert DataFrame to list of dictionaries
 data = df.to_dict(orient='records')
@@ -18,4 +24,5 @@ print(data)
 
 # Write to YAML file with ordered columns
 with open(yaml_file, mode='w') as outfile:
+    outfile.write("# do not make any changes to this file\n")
     yaml.dump(data, outfile, default_flow_style=False, sort_keys=False)
